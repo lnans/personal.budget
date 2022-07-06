f5b1c2f9256439ea828ecc35703c82fc0452a14e
Nans Laupretre <nans.laupretre@gmail.com>
2022-07-06 16:06:32 +0200

    chore - fix sonarLint

--- src/Domain/Exceptions/NotFoundException.cs
+++ src/Domain/Exceptions/NotFoundException.cs
@@ -1,9 +1,15 @@
+using System.Runtime.Serialization;
+
 namespace Domain.Exceptions;
 
 [Serializable]
 public class NotFoundException : Exception
 {
     public NotFoundException(string message) : base(message)
+    {
+    }
+
+    protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
 }
