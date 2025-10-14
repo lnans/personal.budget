76e05cd630f19965c1269504e8b7bda2833f6e37
Nans Laupretre <nans.laupretre@gmail.com>
2025-10-14 22:30:45 +0200

    chore(Infra): add Guard clause and dependency config

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,30 @@
+using Application.Interfaces;
+using Ardalis.GuardClauses;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Infrastructure;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static void AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+        Guard.Against.NullOrEmpty(connectionString, nameof(connectionString));
+
+        services.ConfigurePersistence(connectionString);
+    }
+
+    private static void ConfigurePersistence(
+        this IServiceCollection services,
+        string connectionString
+    ) =>
+        services.AddDbContext<IAppDbContext, AppDbContext>(config =>
+            config.UseNpgsql(connectionString)
+        );
 }
