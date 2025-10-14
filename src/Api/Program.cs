74594be130beeb76e321a935d208b69c6e8644bc
Nans Laupretre <nans.laupretre@gmail.com>
2025-10-14 23:19:04 +0200

    chore(Api): migrate database on startup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,6 @@
 using Application;
 using Infrastructure;
+using Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(configuration);
 
 var app = builder.Build();
+
+await app.Services.InitialiseDatabaseAsync();
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ src/Infrastructure/Persistence/AppDbContextInitializer.cs
@@ -0,0 +1,55 @@
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Persistence;
+
+public static class InitializerExtensions
+{
+    public static async Task InitialiseDatabaseAsync(
+        this IServiceProvider provider
+    )
+    {
+        using var scope = provider.CreateScope();
+
+        var initializer =
+            scope.ServiceProvider.GetRequiredService<AppDbContextInitializer>();
+
+        await initializer.InitialiseAsync();
+    }
+}
+
+internal class AppDbContextInitializer
+{
+    private readonly AppDbContext _dbContext;
+    private readonly ILogger<AppDbContextInitializer> _logger;
+
+    public AppDbContextInitializer(
+        ILogger<AppDbContextInitializer> logger,
+        AppDbContext dbContext
+    )
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_dbContext.Database.IsNpgsql())
+            {
+                await _dbContext.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "An error occurred while initialising the database."
+            );
+            throw;
+        }
+    }
+}
