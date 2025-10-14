68157ba423d89bd77b42299f3ff289958149249b
Nans Laupretre <nans.laupretre@gmail.com>
2025-10-14 22:45:02 +0200

    chore(Infra): configura application and infra services

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,13 @@
+using Application;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
