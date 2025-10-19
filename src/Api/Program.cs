c18cb59bef6144c48b1c7c1cd289f6f2455b079a
Nans Laupretre <nans.laupretre@gmail.com>
2025-10-19 11:04:38 +0200

    chore(Api): add Serilog logging to Console

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,30 +3,47 @@ using Api.Configurations;
 using Application;
 using Infrastructure;
 using Infrastructure.Persistence;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-var configuration = builder.Configuration;
-var services = builder.Services;
-
-services.AddApiServices();
-services.AddApplicationServices(configuration);
-services.AddInfrastructureServices(configuration);
-
-var app = builder.Build();
-
-app.UseDeveloperExceptionPage();
-app.UseCors(config =>
+try
 {
-    config.AllowAnyHeader();
-    config.AllowAnyOrigin();
-    config.AllowAnyMethod();
-    config.WithExposedHeaders("Content-Disposition");
-});
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
+    var services = builder.Services;
 
-app.MapOpenApiEndpoints();
-app.MapApiEndpoints();
+    builder.Host.UseSerilog(
+        (ctx, sv, config) => config.ReadFrom.Configuration(ctx.Configuration).ReadFrom.Services(sv)
+    );
 
-await app.Services.InitialiseDatabaseAsync();
+    services.AddApiServices();
+    services.AddApplicationServices(configuration);
+    services.AddInfrastructureServices(configuration);
 
-app.Run();
+    var app = builder.Build();
+
+    app.UseDeveloperExceptionPage();
+    app.UseCors(config =>
+    {
+        config.AllowAnyHeader();
+        config.AllowAnyOrigin();
+        config.AllowAnyMethod();
+        config.WithExposedHeaders("Content-Disposition");
+    });
+
+    app.MapOpenApiEndpoints();
+    app.MapApiEndpoints();
+
+    await app.Services.InitialiseDatabaseAsync();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
