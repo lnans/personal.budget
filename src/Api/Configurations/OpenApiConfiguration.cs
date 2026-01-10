4d55e52d702bdf42d32bb1e1d2826365136bb734
Nans Laupretre <nans.laupretre@gmail.com>
2026-01-10 20:04:32 +0100

    chore(Packages): update to dotnet 10, fix open api default authType

--- src/Api/Configurations/OpenApiConfiguration.cs
+++ src/Api/Configurations/OpenApiConfiguration.cs
@@ -7,6 +7,17 @@ public static class OpenApiConfiguration
     public static void MapOpenApiEndpoints(this WebApplication app)
     {
         app.MapOpenApi().AllowAnonymous();
-        app.MapScalarApiReference("/docs", config => config.Title = "Budget.Api").AllowAnonymous();
+        app.MapScalarApiReference(
+                "/docs",
+                config =>
+                {
+                    config.Title = "Budget.Api";
+                    config.Authentication = new ScalarAuthenticationOptions()
+                    {
+                        PreferredSecuritySchemes = new List<string>() { "Bearer" },
+                    };
+                }
+            )
+            .AllowAnonymous();
     }
 }
